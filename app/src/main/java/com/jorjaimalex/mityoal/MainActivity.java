ba82ba35b4faccc70152ab8e78f6100154757c70
Darikots <jaimedlrosa@gmail.com>
2021-01-29 12:18:58 +0100

    Merge branch 'master' of D:\Documentos\Interfaces\MitYoAl with conflicts.

--- app/src/main/java/com/jorjaimalex/mityoal/MainActivity.java
+++ app/src/main/java/com/jorjaimalex/mityoal/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    oooooooo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
