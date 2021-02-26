d1048d5657779620fa0e74de7a4ad7726502c2cf
Alejandrom330 <alejandro.mateo.pereira@gmail.com>
2021-02-26 11:41:29 +0100

    Añadiendo bottom app bar

--- app/src/main/java/com/jorjaimalex/mityoal/MainActivity.java
+++ app/src/main/java/com/jorjaimalex/mityoal/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.bottom_app_bar, menu);
+        getMenuInflater().inflate(R.menu.bottom_app_bar_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
